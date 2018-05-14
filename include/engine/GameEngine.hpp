fd65625fa1af4b64d65ea1448c54f62e4d87cd31
Tom <tom.rochat@epitech.eu>
2018-05-14 19:09:03 +0200

    Added blueprint for GameEngine class

---
+++ include/engine/GameEngine.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2018
+** indie
+** File description:
+** Game Engine
+*/
+
+#pragma once
+
+namespace ids {
+
+	class GameEngine {
+		public:
+		GameEngine();
+		~GameEngine() noexcept = default;
+	};
+
+}
---
+++ srcs/engine/GameEngine.cpp
@@ -0,0 +1,8 @@
+/*
+** EPITECH PROJECT, 2018
+** indie
+** File description:
+** Game Engine
+*/
+
+#include "engine/GameEngine.hpp"
