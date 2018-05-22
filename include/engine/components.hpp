8d8393decc26100ff8bd117e882cb5038cc879b8
Tom <tom.rochat@epitech.eu>
2018-05-22 12:18:51 +0200

    Add components
    > Add components file in which all components will be located

---
+++ include/engine/components.hpp
@@ -0,0 +1,57 @@
+/*
+** EPITECH PROJECT, 2018
+** indie
+** File description:
+** indie
+*/
+
+#pragma once
+
+#include <cstddef>
+
+enum ActionTarget {
+	INVINCIBILITY,
+	MAXBOMBS,
+	FOOTPOWERUP,
+	POWER,
+};
+
+struct Character {
+	bool invincibility;
+	bool footPowerUp;
+	std::size_t power;
+	std::size_t maxBombs;
+};
+
+struct Explosion {
+	std::size_t power;
+	std::size_t timeout;
+};
+
+struct Collectible {
+	ActionTarget action;
+};
+
+struct Velocity {
+	float x;
+	float y;
+};
+
+struct Position {
+	float x;
+	float y;
+};
+
+struct Input {
+	bool goLeft;
+	bool goRight;
+	bool goUp;
+	bool goDown;
+};
+
+struct AiInput {
+	bool goLeft;
+	bool goRight;
+	bool goUp;
+	bool goDown;
+};
