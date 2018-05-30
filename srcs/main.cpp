c09ca77a549ae293d10d15129dbf9e150f146fbc
Thomas Burgaud <thomas.burgaud@epitech.eu>
2018-05-30 13:45:24 +0200

    Add shadow

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -8,23 +8,26 @@
 #include <string>
 #include <iostream>
 #include "settingManager/settingManager.hpp"
-#include "gfx/Renderable.hpp"
 #include "gfx/Renderer.hpp"
 
 int main()
 {
 	std::string filename("../../settings/settings.xml");
+	irr::core::stringw assetPath("/home/zombre/tek2/cpp_indie_studio/assets/meshs/ground.obj");
 	gfx::Renderer window;
-	gfx::Renderable asset;
-	opt::settingManager settings(filename);
-	vec2d<int> pos(0,0);
-	irr::core::rect<irr::s32> size(0,0,100,500);
+	vec3d<float> position(10.f, 100.f, 0.f);
+	vec3d<float> positionlego(10,3,0);
+	vec3d<float> size(0.f, 0.f, 0.f);
+	vec3d<float> rotation(45.f, 0.f, 0.f);
 
-	settings.load();
-	window.addArchive(settings.getValue("archive"));
-	//asset.setMesh(window.getScene(), settings.getValue("mesh"))
-	asset.setAnimatedMesh(window.getScene(), "../../assets/meshs/ground.obj");
-	asset.setPosition(vec3d<float>(150, 150, 150));
+	irr::core::array<irr::SJoystickInfo> joystickInfo;
+
+	gfx::idSprite id;
+	position = {0,0,0};
+	id = window.createb3dMesh("/home/zombre/tek2/cpp_indie_studio/assets/meshs/perso.b3d");
+	window.setPosition(id, positionlego);
+	id = window.createMesh(assetPath);
+	window.setPosition(id, positionlego);
 	while (window.isRun()) {
 		if (window.pollEvent() == ids::QUIT) {
 			break;
