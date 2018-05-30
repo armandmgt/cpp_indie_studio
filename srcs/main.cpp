9db9989a57ee457ebbae045cd1dd78731c5c8b4f
armandmgt <armand.megrot@epitech.eu>
2018-05-30 19:54:18 +0200

    now renders a brick with good lighting

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -12,10 +12,10 @@
 int main()
 {
 	std::string filename("../../settings/settings.xml");
-	irr::core::stringw assetPath("/home/zombre/tek2/cpp_indie_studio/assets/meshs/ground.obj");
+	irr::core::stringw assetPath("../../assets/meshs/ground.obj");
 	gfx::Renderer window;
 	vec3d<float> position(10.f, 100.f, 0.f);
-	vec3d<float> positionlego(10,3,0);
+	vec3d<float> positionlego(10, 0, 10);
 	vec3d<float> size(0.f, 0.f, 0.f);
 	vec3d<float> rotation(45.f, 0.f, 0.f);
 
@@ -23,8 +23,8 @@ int main()
 
 	gfx::idSprite id;
 	position = {0,0,0};
-	id = window.createb3dMesh("/home/zombre/tek2/cpp_indie_studio/assets/meshs/perso.b3d");
-	window.setPosition(id, positionlego);
+//	id = window.createb3dMesh("../../assets/meshs/perso.b3d");
+//	window.setPosition(id, positionlego);
 	id = window.createMesh(assetPath);
 	window.setPosition(id, positionlego);
 	while (window.isRun()) {
