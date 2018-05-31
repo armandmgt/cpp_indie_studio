41ef64599b66db123afb7137722222dcacfc65bf
Tom <tom.roch4t@gmail.com>
2018-05-31 17:11:39 +0200

    Refactor for Map.hpp
    > Rename private methods
    > Changed dtor

--- include/map/Map.hpp
+++ include/map/Map.hpp
@@ -5,7 +5,6 @@
 **        Map generation
 */
 
-#include <list>
 #include <vector>
 #include <string>
 #include <iostream>
@@ -21,7 +20,7 @@ typedef enum direction {
 typedef enum item {
 	BREAKABLE_WALL = '*',
 	BOMB = 'O',
-	WALL_PASS = '|',
+	KICK_POWERUP = '|',
 	SPEED_UP = 'S',
 	BOMB_UP = 'B',
 	FIRE_UP = 'F',
@@ -37,8 +36,9 @@ class Map
 {
 public:
 	Map();
-	Map(size_t, size_t);
-	~Map();
+	Map(size_t l, size_t w);
+	~Map() noexcept = default;
+
 	void initMap(size_t);
 	std::vector<std::string> &getMap();
 	void printMap() const;
@@ -49,18 +49,18 @@ private:
 	std::vector<std::string> _map;
 	size_t _length;
 	size_t _width;
-	bool addWall(size_t, size_t);
-	void fillMap();
-	void fillLine(std::string &);
-	void proceduralGen(size_t);
-	void digTopLeftCorner();
-	void digTopRightCorner();
-	void digBottomLeftCorner();
-	void digBottomRightCorner();
+	bool _addWall(size_t, size_t);
+	void _fillMap();
+	void _fillLine(std::string &);
+	void _proceduralGen(size_t);
+	void _digTopLeftCorner();
+	void _digTopRightCorner();
+	void _digBottomLeftCorner();
+	void _digBottomRightCorner();
 
 	std::map<int, item> _gamble = {
 		{40, BREAKABLE_WALL},
-		{70, WALL_PASS},
+		{70, KICK_POWERUP},
 		{80, SPEED_UP},
 		{90, BOMB_UP},
 		{100, FIRE_UP}
