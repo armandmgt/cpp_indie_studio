c57f3aab8fd0c209fe55b208350f034a3efc73da
Thomas Burgaud <thomas.burgaud@epitech.eu>
2018-05-31 16:28:34 +0200

    Start of create entity

--- srcs/world/World.cpp
+++ srcs/world/World.cpp
@@ -4,4 +4,21 @@
 ** File description:
 ** world
 */
+#include <engine/Components.hpp>
 #include "world/World.hpp"
+
+ecs::entityId ecs::world::createEntity(ecs::entityType type)
+{
+	static std::unordered_map<entityType, std::bitset<Entity::bitSize>> map {
+		{PLAYER, },
+		{POWERUP, },
+		{BOMB, },
+		{WALL, },
+		{FLAMME, }
+	};
+	return _world.size() - 1;
+}
+
+void ecs::world::destroyEntity(ecs::entityId id)
+{
+}
