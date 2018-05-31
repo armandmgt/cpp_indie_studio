bd25e6d90f833ccfa7cc2c02a184d7b0c6aa9afc
Thomas Burgaud <thomas.burgaud@epitech.eu>
2018-05-31 19:05:54 +0200

    commit for checkout

--- srcs/world/World.cpp
+++ srcs/world/World.cpp
@@ -5,21 +5,11 @@
 ** world
 */
 #include <engine/Components.hpp>
+#include <functional>
 #include "world/World.hpp"
 
 namespace ecs {
-	entityId world::createEntity(entityType type) {
-		static std::unordered_map<entityType, std::bitset<Entity::bitSize>> map {
-			{PLAYER, COMP_POSITION | COMP_VELOCITY | COMP_CHARACTER | COMP_DESTRUCTIBLE},
-			{POWERUP, COMP_POSITION | COMP_COLLECTIBLE},
-			{BOMB, COMP_VELOCITY | COMP_EXPLOSION},
-			{WALL, COMP_POSITION | COMP_DESTRUCTIBLE},
-			{FLAMME, COMP_POSITION}
-		};
-		return _world.size() - 1;
-	}
-
-	void world::destroyEntity(entityId id) {
+		void world::destroyEntity(entityId id) {
 	}
 
 	void world::_spawnEntitiesFromMap(std::vector<std::string> &&gameMap) {
