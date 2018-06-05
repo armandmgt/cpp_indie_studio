fda4ad1840f1d2127ef27328977e3ada6131db29
Thomas Burgaud <thomas.burgaud@epitech.eu>
2018-06-05 22:53:00 +0200

    Push Ctor and Dtor

--- include/engine/Components.hpp
+++ include/engine/Components.hpp
@@ -38,6 +38,12 @@ namespace ecs {
 
 	struct Character : public Component {
 		static comp getType() { return COMP_CHARACTER; };
+		Character(bool i, bool f, std::size_t p , std::size_t m) : invincibility(i),
+									   footPowerUp(f),
+									   power(p),
+									   maxBombs(m)
+		{};
+		~Character() = default;
 		bool invincibility;
 		bool footPowerUp;
 		std::size_t power;
@@ -46,6 +52,9 @@ namespace ecs {
 
 	struct Explosion : public Component {
 		static comp getType() { return COMP_EXPLOSION; };
+		Explosion(std::size_t p, std::size_t t) : power(p), timeout(t)
+		{};
+		~Explosion() = default;
 		std::size_t power;
 		std::size_t timeout;
 	};
@@ -55,10 +64,14 @@ namespace ecs {
 		{};
 		static comp getType() { return COMP_COLLECTIBLE; };
 		Collectible() = default;
+		~Collectible() = default;
 		ActionTarget action;
 	};
 
 	struct Velocity : public Component {
+		Velocity(float w, float v) : x(w), y(v)
+		{};
+		~Velocity() = default;
 		static comp getType() { return COMP_VELOCITY; };
 		float x;
 		float y;
@@ -66,17 +79,26 @@ namespace ecs {
 
 	struct Orientation : public Component{
 		static comp getType() { return COMP_ORIENTATION; };
+		explicit Orientation(float ori) : orientation(ori)
+		{};
+		~Orientation() = default;
 		float orientation;
 	};
 
 	struct Position : public Component {
 		static comp getType() { return COMP_POSITION; };
+		Position(float d, float v) : x(d), y(v)
+		{};
+		~Position() = default;
 		float x;
 		float y;
 	};
 
 	struct Input : public Component {
 		static comp getType() { return COMP_INPUT; };
+		Input(bool l, bool r, bool u, bool d) : goLeft(l), goRight(r), goUp(u), goDown(d)
+		{};
+		~Input() = default;
 		bool goLeft;
 		bool goRight;
 		bool goUp;
@@ -85,6 +107,9 @@ namespace ecs {
 
 	struct AiInput : public Component {
 		static comp getType() { return COMP_AIINPUT; };
+		AiInput(bool l, bool r, bool u, bool d) : goLeft(l), goRight(r), goUp(u), goDown(d)
+		{};
+		~AiInput() = default;
 		bool goLeft;
 		bool goRight;
 		bool goUp;
@@ -99,6 +124,9 @@ namespace ecs {
 
 	struct Graphic : public Component {
 		static comp getType() { return COMP_GRAPHIC; };
+		explicit Graphic(irr::scene::ISceneNode *s) : sceneNode(s)
+		{};
+		~Graphic() = default;
 		irr::scene::ISceneNode *sceneNode;
 	};
 }
