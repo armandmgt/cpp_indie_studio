8f711497969d9850f45a67760ba7150ad68575e1
armandmgt <armand.megrot@epitech.eu>
2018-06-10 17:26:15 +0200

    Fix ifndef in Pause header

--- include/menu/Pause.hpp
+++ include/menu/Pause.hpp
@@ -8,19 +8,19 @@
 #ifndef CPP_INDIE_STUDIO_PAUSE_HPP
 #define CPP_INDIE_STUDIO_PAUSE_HPP
 
-#endif //CPP_INDIE_STUDIO_PAUSE_HPP
-
 #include "menu/AMenu.hpp"
 
-namespace ids { namespace menu {
+namespace ids {
+	namespace menu {
+		class Pause : public AMenu {
+		public:
+			Pause(gfx::Renderer *renderer, Music *music);
+			~Pause();
+			sceneId run() override;
+		private:
+			evt::MyEventReceiver &_events;
+		};
+	}
+}
 
-	class Pause : public AMenu {
-	public:
-		Pause(gfx::Renderer *renderer, Music *music);
-		~Pause();
-		sceneId run() override;
-	private:
-		evt::MyEventReceiver &_events;
-	};
-
-}}
+#endif //CPP_INDIE_STUDIO_PAUSE_HPP
